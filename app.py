8680d0ef13195b66ae42d49faa3cfdf6c62c314c
Md Sami Uddin <ms2228470@gmail.com>
2026-03-13 00:07:58 +0530

    Added home route for API

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def extract_features(url):
         1 if "account" in url.lower() else 0
     ]
     return [features]
+@app.route("/")
+def home():
+    return "phishing ML API Running successfully"
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
@@ -23,6 +27,12 @@ def predict():
     probability = model.predict_proba(features)[0][1]
     # Convert probability to percentage
     confidence = round(probability * 100, 2)
+    # Adjust confidence ranges for realism
+    if confidence < 30:
+    confidence = round(confidence * 0.8, 2)
+    elif confidence > 80:
+    confidence = round(confidence * 1.05, 2)
+    confidence = min(confidence, 99.9)
     if prediction == 1:
         result = "Phishing Website"
     else:
