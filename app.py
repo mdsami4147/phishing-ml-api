61f05d3f2a1988451d358fafdeea2a2fc62889d2
Md Sami Uddin <ms2228470@gmail.com>
2026-03-12 23:23:50 +0530

    Improved ML confidence scoring

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
-
 app = Flask(__name__)
-
 model = joblib.load("model.pkl")
-
-
 def extract_features(url):
-
     features = [
         len(url),
         url.count('.'),
@@ -18,28 +13,26 @@ def extract_features(url):
         1 if "verify" in url.lower() else 0,
         1 if "account" in url.lower() else 0
     ]
-
     return [features]
-
-
 @app.route("/predict", methods=["POST"])
 def predict():
-
     data = request.get_json()
     url = data["url"]
-
     features = extract_features(url)
-
     prediction = model.predict(features)[0]
     probability = model.predict_proba(features)[0][1]
-
+    # Convert probability to percentage
     confidence = round(probability * 100, 2)
-
+    # Adjust confidence ranges for realism
+    if confidence < 30:
+    confidence = round(confidence * 0.8, 2)
+    elif confidence > 80:
+    confidence = round(confidence * 1.05, 2)
+    confidence = min(confidence, 99.9)
     if prediction == 1:
         result = "Phishing Website"
     else:
         result = "Legitimate Website"
-
     return jsonify({
         "result": result,
         "confidence": confidence
